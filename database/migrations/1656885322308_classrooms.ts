6d039912a5c09ee5711463f7f4430f2dda43c570
duardoqueiroz <eduardoldq1@gmail.com>
2022-07-03 20:18:35 -0300

    feat: classroom migration

---
+++ database/migrations/1656885322308_classrooms.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'classrooms'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('class').notNullable()
+      table
+        .integer('teacher_id')
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
