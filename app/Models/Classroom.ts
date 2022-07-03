a8aae15b89ce2e77cb9db52d44cf85bcc8a9689a
duardoqueiroz <eduardoldq1@gmail.com>
2022-07-03 20:18:43 -0300

    feat: classroom model

---
+++ app/Models/Classroom.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Classroom extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public class: string
+
+  @column()
+  public teacherId: number
+
+  @hasMany(() => User)
+  public students: HasMany<typeof User>
+
+  @belongsTo(() => User, {
+    foreignKey: 'teacherId',
+  })
+  public teacher: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
