c2da96cdbf40db8ff7f41bad5e25fc2daae1be40
daviromao <davi.romao0210@gmail.com>
2022-07-04 19:13:58 -0300

    feat: create post policies

---
+++ app/Policies/PostPolicy.ts
@@ -0,0 +1,20 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import User from 'App/Models/User'
+import Post from 'App/Models/Post'
+
+export default class PostPolicy extends BasePolicy {
+	public async update(user: User, post: Post) {
+    if(user.id === post.user.id){
+      return true
+    }
+
+    return false
+  }
+	public async delete(user: User, post: Post) {
+    if(user.id === post.user.id){
+      return true
+    }
+
+    return false
+  }
+}
